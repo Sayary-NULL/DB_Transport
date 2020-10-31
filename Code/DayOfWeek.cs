ea358e5b877cfd4d43291a9a5782fc28750101f4
Shliakhtin Kirill <33820978+Sayary-NULL@users.noreply.github.com>
2020-10-31 11:34:02 +0300

    Добавлен оператор для подсчета планируемого пробега

--- Code/DayOfWeek.cs
+++ Code/DayOfWeek.cs
@@ -17,5 +17,30 @@ namespace Test1.Code
         public List<double> ELC { get; set; }
         [JsonIgnore]
         public string JsonSTR { get; set; }
+
+        public static DayOfWeek operator *(DayOfWeek a11, double a2)
+        {
+            var a1 = new DayOfWeek
+            {
+                SC = new List<double>(a11.SC),
+                MC = new List<double>(a11.MC),
+                LC = new List<double>(a11.LC),
+                ESC = new List<double>(a11.ESC),
+                ELC = new List<double>(a11.ELC)
+            };
+
+            if (a1.SC != null)
+                a1.SC[1] *= a2;
+            if (a1.MC != null)
+                a1.MC[1] *= a2;
+            if (a1.LC != null)
+                a1.LC[1] *= a2;
+            if (a1.ESC != null)
+                a1.ESC[1] *= a2;
+            if (a1.ELC != null)
+                a1.ELC[1] *= a2;
+
+            return a1;
+        }
     }
 }
