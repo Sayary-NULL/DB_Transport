626bab2d53548c34fc65a48bedef9e9daeac5d17
Shliakhtin Kirill <33820978+Sayary-NULL@users.noreply.github.com>
2020-10-31 11:32:38 +0300

    Добавлены месяцы для приложения 3

--- Code/StaticValues.cs
+++ Code/StaticValues.cs
@@ -9,6 +9,7 @@ namespace Test1.Code
     public static class StaticValues
     {
         public static string[] months = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" };
+        public static string[] monthsmod = { "Январь", "Февраль", "Март", "Апрель 1/2", "Апрель 1/2", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь 1/2", "Октябрь 1/2", "Ноябрь", "Декабрь" };
         public static string IPAdress { get; set; }
         public static string Login { get; set; }
         public static string Password { get; set; }
