0fd82dce32768e4ff692d1c65d547229045d6e9f
Shliakhtin Kirill <33820978+Sayary-NULL@users.noreply.github.com>
2020-10-31 11:33:38 +0300

    В контракт добавлен ИНН подрядчика

--- Code/Contract.cs
+++ Code/Contract.cs
@@ -14,15 +14,17 @@ namespace Test1.Code
         public DateTimeOffset With { get; set; }
         public DateTimeOffset By { get; set; }
         public SqlMoney money { get; set; }
+        public long INN { get; set; }
 
         public string[] ToRow()
         {
-            string[] rez = new string[5];
+            string[] rez = new string[6];
             rez[0] = ID.ToString();
             rez[1] = Nomber_Contract;
-            rez[2] = With.ToString("dd.MM.YYYY");
-            rez[3] = By.ToString("dd.MM.YYYY");
+            rez[2] = With.ToString("dd.MM.yyyy");
+            rez[3] = By.ToString("dd.MM.yyyy");
             rez[4] = money.ToString();
+            rez[5] = INN.ToString();
 
             return rez;
         }
