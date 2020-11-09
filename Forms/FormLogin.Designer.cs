a81aece513597f196c7db42371effaf72b69113a
Shliakhtin Kirill <33820978+Sayary-NULL@users.noreply.github.com>
2020-11-09 15:08:27 +0300

    Добавлена возможность запоминать последний успешный вход в систему

--- Forms/FormLogin.Designer.cs
+++ Forms/FormLogin.Designer.cs
@@ -28,93 +28,110 @@
     /// </summary>
     private void InitializeComponent()
     {
-        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
-        this.tbIP = new System.Windows.Forms.TextBox();
-        this.tbLogin = new System.Windows.Forms.TextBox();
-        this.tbPass = new System.Windows.Forms.TextBox();
-        this.label1 = new System.Windows.Forms.Label();
-        this.label2 = new System.Windows.Forms.Label();
-        this.label3 = new System.Windows.Forms.Label();
-        this.button1 = new System.Windows.Forms.Button();
-        this.SuspendLayout();
-        // 
-        // tbIP
-        // 
-        this.tbIP.Location = new System.Drawing.Point(12, 42);
-        this.tbIP.Name = "tbIP";
-        this.tbIP.Size = new System.Drawing.Size(156, 20);
-        this.tbIP.TabIndex = 0;
-        // 
-        // tbLogin
-        // 
-        this.tbLogin.Location = new System.Drawing.Point(12, 112);
-        this.tbLogin.Name = "tbLogin";
-        this.tbLogin.Size = new System.Drawing.Size(156, 20);
-        this.tbLogin.TabIndex = 1;
-        // 
-        // tbPass
-        // 
-        this.tbPass.Location = new System.Drawing.Point(12, 181);
-        this.tbPass.Name = "tbPass";
-        this.tbPass.Size = new System.Drawing.Size(156, 20);
-        this.tbPass.TabIndex = 2;
-        // 
-        // label1
-        // 
-        this.label1.AutoSize = true;
-        this.label1.Location = new System.Drawing.Point(9, 26);
-        this.label1.Name = "label1";
-        this.label1.Size = new System.Drawing.Size(52, 13);
-        this.label1.TabIndex = 3;
-        this.label1.Text = "IP Adress";
-        // 
-        // label2
-        // 
-        this.label2.AutoSize = true;
-        this.label2.Location = new System.Drawing.Point(9, 96);
-        this.label2.Name = "label2";
-        this.label2.Size = new System.Drawing.Size(33, 13);
-        this.label2.TabIndex = 4;
-        this.label2.Text = "Login";
-        // 
-        // label3
-        // 
-        this.label3.AutoSize = true;
-        this.label3.Location = new System.Drawing.Point(9, 165);
-        this.label3.Name = "label3";
-        this.label3.Size = new System.Drawing.Size(53, 13);
-        this.label3.TabIndex = 5;
-        this.label3.Text = "Password";
-        // 
-        // button1
-        // 
-        this.button1.Location = new System.Drawing.Point(52, 223);
-        this.button1.Name = "button1";
-        this.button1.Size = new System.Drawing.Size(75, 23);
-        this.button1.TabIndex = 6;
-        this.button1.Text = "Вход";
-        this.button1.UseVisualStyleBackColor = true;
-        this.button1.Click += new System.EventHandler(this.button1_Click);
-        // 
-        // FormLogin
-        // 
-        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(189, 260);
-        this.Controls.Add(this.button1);
-        this.Controls.Add(this.label3);
-        this.Controls.Add(this.label2);
-        this.Controls.Add(this.label1);
-        this.Controls.Add(this.tbPass);
-        this.Controls.Add(this.tbLogin);
-        this.Controls.Add(this.tbIP);
-        this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-        this.MaximizeBox = false;
-        this.MinimizeBox = false;
-        this.Name = "FormLogin";
-        this.Text = "FormLogin";
-        this.ResumeLayout(false);
-        this.PerformLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormLogin));
+            this.tbIP = new System.Windows.Forms.TextBox();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.tbPass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // tbIP
+            // 
+            this.tbIP.Location = new System.Drawing.Point(12, 42);
+            this.tbIP.Name = "tbIP";
+            this.tbIP.Size = new System.Drawing.Size(156, 20);
+            this.tbIP.TabIndex = 0;
+            this.tbIP.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbIP_KeyDown);
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.Location = new System.Drawing.Point(12, 112);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(156, 20);
+            this.tbLogin.TabIndex = 1;
+            this.tbLogin.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbLogin_KeyDown);
+            // 
+            // tbPass
+            // 
+            this.tbPass.Location = new System.Drawing.Point(12, 151);
+            this.tbPass.Name = "tbPass";
+            this.tbPass.Size = new System.Drawing.Size(156, 20);
+            this.tbPass.TabIndex = 2;
+            this.tbPass.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbPass_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "IP Adress";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Login";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Password";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(53, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Вход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 177);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(162, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Запомнить логин и пароль";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(189, 272);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPass);
+            this.Controls.Add(this.tbLogin);
+            this.Controls.Add(this.tbIP);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
     }
 
@@ -127,5 +144,6 @@
     private System.Windows.Forms.Label label2;
     private System.Windows.Forms.Label label3;
     private System.Windows.Forms.Button button1;
-}
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
 }
--- Forms/FormLogin.cs
+++ Forms/FormLogin.cs
@@ -18,6 +18,13 @@ namespace Test1.Forms
         public FormLogin()
         {
             InitializeComponent();
+            if(Settings1.Default.EnableSaveLogin)
+            {
+                tbLogin.Text = Settings1.Default.Login;
+                tbPass.Text = Settings1.Default.Password;
+                tbIP.Text = Settings1.Default.Adress;
+                checkBox1.Checked = true;
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -33,12 +40,51 @@ namespace Test1.Forms
                     Test1.Code.StaticValues.IPAdress = tbIP.Text;
                     Test1.Code.StaticValues.Login = tbLogin.Text;
                     Test1.Code.StaticValues.Password = tbPass.Text;
+
+                    if(checkBox1.Checked)
+                    {
+                        //MessageBox.Show(this, "Лучший способ сохранить это записать, записывайте, я подожду.", "Инфо", MessageBoxButtons.OK, MessageBoxIcon.Question);
+                        Settings1.Default.Login = tbLogin.Text;
+                        Settings1.Default.Password = tbPass.Text;
+                        Settings1.Default.Adress = tbIP.Text;
+                        Settings1.Default.EnableSaveLogin = true;
+                    }
+                    else Settings1.Default.EnableSaveLogin = false;
+                    Settings1.Default.Save();
+
                     this.Close();
                 }
                 catch
                 {
                     MessageBox.Show(this, "Ошибка IP адресса или Имя пользователя или Пароля", "Ошибка входа", MessageBoxButtons.OK, MessageBoxIcon.Information);
                 }
+            }
+        }
+
+        private void tbIP_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                tbLogin.Focus();
+            }
+        }
+
+        private void tbLogin_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                tbPass.Focus();
+            }
+        }
+
+        private void tbPass_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true;
+                button1_Click(sender, new EventArgs());
             }
         }
     }
