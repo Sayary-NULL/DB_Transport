931e6c14be5a08f4736fa2d395445fae8012e0a6
Shliakhtin Kirill <33820978+Sayary-NULL@users.noreply.github.com>
2020-11-21 14:02:20 +0300

    Убраны не использвуемы столбцы

--- Code/Rout.cs
+++ Code/Rout.cs
@@ -10,8 +10,6 @@ namespace Test1.Code
     {
         public int ID { get; set; }
         public int ID_History { get; set; }
-        public int ID_Contract { get; set; }
-        public string Registr { get; set; }
         public string Poryd { get; set; }
         public string TypeOnOut { get; set; }
         public string TypeOfRegular { get; set; }
@@ -20,17 +18,15 @@ namespace Test1.Code
 
         public string[] ToRow()
         {
-            string[] rez = new string[9];
+            string[] rez = new string[7];
 
             rez[0] = ID.ToString();
             rez[1] = ID_History.ToString();
-            rez[2] = ID_Contract.ToString();
-            rez[3] = Registr;
-            rez[4] = Poryd;
-            rez[5] = TypeOnOut;
-            rez[6] = TypeOfRegular;
-            rez[7] = Name;
-            rez[8] = Type_communication;
+            rez[2] = Poryd;
+            rez[3] = TypeOnOut;
+            rez[4] = TypeOfRegular;
+            rez[5] = Name;
+            rez[6] = Type_communication;
 
             return rez;
         }
